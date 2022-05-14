de49137b97b32184a0ff2ae02a135e8f8b3f6df6
unknown <huenk105@gmail.com>
2022-05-14 11:48:05 +0700

    demo

---
+++ src/ReadFileExample.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadFileExample{
+    public void readFileText(String filePath){
+        try {
+            File file = new File(filePath);
+            if (!file.exists()){
+                throw new FileNotFoundException();
+            }
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            int sum = 0;
+            while ((line = br.readLine())!=null){
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+            System.out.println("Tong = "+sum);
+        } catch (Exception e) {
+            System.err.println("File khong ton tai or noi dung co loi");
+        }
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("Nhap duong dan file: ");
+//        Scanner scanner = new Scanner(System.in);
+//        String path = scanner.nextLine();
+
+        ReadFileExample readFileEx = new ReadFileExample();
+        readFileEx.readFileText("Number.txt");
+    }
+}
